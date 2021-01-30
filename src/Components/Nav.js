2545f3881a8cbf451183f42a4af567bde21f114b
yang <hello.git.fila3894@naver.com>
2021-01-30 23:17:45 +0900

    네비게이션바

---
+++ src/Components/Nav.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {
+    Button,
+    Navbar,
+    Nav,
+} from 'react-bootstrap';
+//css
+import '../Css/Nav.css'
+
+// NavBar
+function 네비게이션바(){
+    return(
+      <div className="네비게이션">
+        <Navbar bg="light" expand="lg">
+          <Navbar.Brand href="/">Yang PortFolio</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="mr-auto">
+              <Link to="/"><Button variant="white">홈</Button></Link>
+              <Link to="/portfolio"><Button variant="white">포트폴리오</Button></Link>
+              <Link to="/description"><Button variant="white">자기소개</Button></Link>
+              <a href="https://github.com/fila3894"><Button variant="success">GitHub</Button></a>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+  
+  export default 네비게이션바;
