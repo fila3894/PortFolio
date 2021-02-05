4ed8d76615da9d492d43b2d6b9c4066da6177af6
맡은 일에 최선을 다하는 개발자 양지훈입니다! <70878920+fila3894@users.noreply.github.com>
2021-02-06 02:10:11 +0900

    네비게이션바

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -12,14 +12,14 @@ import '../Css/Nav.css'
 function 네비게이션바(){
     return(
       <div className="네비게이션">
-        <Navbar bg="light" expand="lg">
-          <Navbar.Brand href="/">Yang PortFolio</Navbar.Brand>
+        <Navbar bg="dark" expand="lg">
+          <Navbar.Brand href="/"><font color="white">Yang PortFolio</font></Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-              <Link to="/"><Button variant="white">홈</Button></Link>
-              <Link to="/portfolio"><Button variant="white">포트폴리오</Button></Link>
-              <Link to="/description"><Button variant="white">자기소개</Button></Link>
+              <Link to="/"><Button variant="dark">홈</Button></Link>
+              <Link to="/portfolio"><Button variant="dark">포트폴리오</Button></Link>
+              <Link to="/description"><Button variant="dark">자기소개</Button></Link>
               <a href="https://github.com/fila3894"><Button variant="success">GitHub</Button></a>
             </Nav>
           </Navbar.Collapse>
